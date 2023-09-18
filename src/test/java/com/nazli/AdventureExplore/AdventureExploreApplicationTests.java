b5692b693a6b70c4ef1d317111da389271942d68
Nazli <nazlifard@gmail.com>
2023-09-18 10:32:34 -0700

    Initial Commit

---
+++ src/test/java/com/nazli/AdventureExplore/AdventureExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazli.AdventureExplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
